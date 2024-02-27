154babaf91d36ad4e542026091d41a8305912aa9
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-02-27 09:28:34 +0700

    feat: add responsive pest monitoring page

--- afms/src/app/components/CardImage.jsx
+++ afms/src/app/components/CardImage.jsx
@@ -1,5 +1,13 @@
-export default function CardImage({ containerName, imageName, handleSave, handleDelete }) {
+import { Roboto_Mono } from "next/font/google";
 
+const robotoMono = Roboto_Mono({ weight: "400", subsets: ["latin"] });
+
+export default function CardImage({
+  containerName,
+  imageName,
+  handleSave,
+  handleDelete,
+}) {
   const formatDate = (datetime) => {
     if (datetime[0] == "n") {
       datetime = datetime.slice(1);
@@ -23,30 +31,40 @@ export default function CardImage({ containerName, imageName, handleSave, handle
   return (
     <div class="w-[11.4rem] h-[20.5rem] md:w-[19rem] md:h-[28rem] bg-[#E5E5E5] border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <img class="rounded-t-lg" src={`https://afmsprototech.blob.core.windows.net/${containerName}/${imageName}`} alt="Pest" />
+        <img
+          class="rounded-t-lg w-[11.4rem] md:w-[19rem] h-[10.2rem] md:h-[16rem]"
+          src={`https://afmsprototech.blob.core.windows.net/${containerName}/${imageName}`}
+          alt="Pest"
+        />
       </a>
       <div class="p-5">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 class="mb-2 text-md md:text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             Date : {formatDate(imageName)}
           </h5>
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 class="mb-2 md:mb-3 text-md md:text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             Time : {formatTime(imageName)}
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 text-center dark:text-gray-400">
-          {imageName[0] === "n" ? "Not Detected" : "Detected"}
+        <p
+          className={`${
+            robotoMono.className
+          } mb-3 md:mb-4 font-normal text-xs md:text-base text-center ${
+            imageName[0] === "n" ? "text-[#FF0000]" : "text-[#00A224]"
+          }`}
+        >
+          {imageName[0] === "n" ? "Wereng not Detected" : "Wereng Detected"}
         </p>
-        <div className="flex space-x-4 justify-center">
+        <div className="flex space-x-2 md:space-x-4 justify-center">
           <a
             href="#"
-            class="inline-flex shadow items-center px-7 py-2 text-sm font-medium text-center text-white bg-[#274C5B] rounded-lg hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="inline-flex shadow items-center px-4 md:px-7 py-2 text-sm font-medium text-center text-white bg-[#274C5B] rounded-lg hover:bg-black"
           >
             Save
           </a>
           <a
             href="#"
-            class="inline-flex shadow items-center px-6 py-2 text-sm font-medium text-center text-black bg-white rounded-lg hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="inline-flex shadow items-center px-3 md:px-6 py-2 text-sm font-medium text-center text-black bg-white rounded-lg hover:bg-black hover:text-white"
           >
             Delete
           </a>
--- afms/src/app/page.jsx
+++ afms/src/app/page.jsx
@@ -23,11 +23,11 @@ export default function Home() {
             height={400}
             alt="Landing Page"
             className={`z-0 ${style.ResponsiveImage}`}
-            // data-aos="fade-right"
+            data-aos="fade-right"
           />
           <div
             className="absolute opacity-90 md:opacity-100 top-6 right-8 ml-6 z-6 pt-6 pr-6 pl-6 pb-6 md:z-10 md:right-12 md:top-12 md:pl-8 md:pr-2 md:pt-8 md:pb-8 bg-white text-[#274C5B] text-wrap rounded-xl flex justify-center items-center md:h-100 md:w-2/5"
-            // data-aos="zoom-in-up"
+            data-aos="zoom-in-up"
           >
             <div className="flex flex-col gap-1 md:gap-2">
               <p
