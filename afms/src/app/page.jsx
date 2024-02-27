708d2bb17d3e01a25f5fa8c6626867286d0d6d53
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-02-27 08:39:07 +0700

    feat: responsive homepage

--- afms/src/app/page.jsx
+++ afms/src/app/page.jsx
@@ -3,6 +3,7 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import CardImage from "./components/CardImage";
+import style from "./page.module.css";
 
 import { Yellowtail } from "next/font/google";
 
@@ -11,7 +12,6 @@ import Image from "next/image";
 const yellowtail = Yellowtail({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
-
   return (
     <>
       <Navbar />
@@ -22,32 +22,56 @@ export default function Home() {
             width={850}
             height={400}
             alt="Landing Page"
-            className="z-0"
-            data-aos="fade-right"
+            className={`z-0 ${style.ResponsiveImage}`}
+            // data-aos="fade-right"
           />
-          <div className="absolute z-10 right-12 top-12 pl-8 pr-2 pt-8 pb-8 bg-white text-[#274C5B] text-wrap rounded-xl flex justify-center items-center h-100 w-2/5" data-aos="zoom-in-up">
-            <div className="flex flex-col gap-2">
-              <p className={`${yellowtail.className} text-2xl text-[#7EB693]`}>
+          <div
+            className="absolute opacity-90 md:opacity-100 top-6 right-8 ml-6 z-6 pt-6 pr-6 pl-6 pb-6 md:z-10 md:right-12 md:top-12 md:pl-8 md:pr-2 md:pt-8 md:pb-8 bg-white text-[#274C5B] text-wrap rounded-xl flex justify-center items-center md:h-100 md:w-2/5"
+            // data-aos="zoom-in-up"
+          >
+            <div className="flex flex-col gap-1 md:gap-2">
+              <p
+                className={`${yellowtail.className} text-base md:text-2xl text-[#7EB693]`}
+              >
                 Eco Friendly
               </p>
-              <h1 className="text-3xl font-extrabold mb-2">
+              <h1 className="text-xl md:text-3xl font-extrabold mb:1 md:mb-2">
                 Automated Farming Management System
               </h1>
               <div>
-                <p className="font-bold">Weather Prediction</p>
-                <p className="text-sm font-light">Sistem akan menampilkan hasil prediksi cuaca dari BMKG yang digunakan untuk melakukan perairan</p>
+                <p className="text-sm md:text-base font-bold">
+                  Weather Prediction
+                </p>
+                <p className="text-xs md:text-sm font-light">
+                  Sistem akan menampilkan hasil prediksi cuaca dari BMKG yang
+                  digunakan untuk melakukan perairan
+                </p>
               </div>
               <div>
-                <p className="font-bold">Soil Moisture Monitoring</p>
-                <p className="text-sm font-light">Sistem akan menampilkan kelembapan data tanah</p>
+                <p className="text-sm md:text-base font-bold">
+                  Soil Moisture Monitoring
+                </p>
+                <p className="text-xs md:text-sm text-sm font-light">
+                  Sistem akan menampilkan kelembapan data tanah
+                </p>
               </div>
               <div>
-                <p className="font-bold">Pest Monitoring</p>
-                <p className="text-sm font-light">Sistem akan menampilkan hasil tangkapan gambar dari ESP32 untuk mendeteksi adanya hama atau tidak</p>
+                <p className="text-sm md:text-base font-bold">
+                  Pest Monitoring
+                </p>
+                <p className="text-xs md:text-sm font-light">
+                  Sistem akan menampilkan hasil tangkapan gambar dari ESP32
+                  untuk mendeteksi adanya hama atau tidak
+                </p>
               </div>
               <div>
-                <p className="font-bold">Servo and LED UV Monitoring</p>
-                <p className="text-sm font-light">SIstem akan menampilkan apakah servo dan LED UV aktif atau tidak</p>
+                <p className="text-sm md:text-base font-bold">
+                  Servo and LED UV Monitoring
+                </p>
+                <p className="text-xs md:text-sm font-light">
+                  Sistem akan menampilkan apakah servo dan LED UV aktif atau
+                  tidak
+                </p>
               </div>
             </div>
           </div>
