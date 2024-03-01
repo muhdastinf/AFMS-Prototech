46e1a3dff960c3061237ac9196c09a6b7cbcf86c
hafidzyami <18221074@std.stei.itb.ac.id>
2024-03-01 20:29:19 +0700

    dahlah nyerah

--- afms/src/app/servo-led-monitoring/page.jsx
+++ afms/src/app/servo-led-monitoring/page.jsx
@@ -5,32 +5,69 @@ import Footer from "../components/Footer";
 import { useState, useEffect } from "react";
 import { Roboto } from "next/font/google";
 import CardComponents from "../components/CardComponents";
+import { Client } from "paho-mqtt";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function ServoLEDMonitor() {
   const [isActiveServo, setIsActiveServo] = useState(false);
   const [isActiveLED, setIsActiveLED] = useState(false);
+  const [targetHumidity, setTargetHumidity] = useState();
+
+  function map(value, inMin, inMax, outMin, outMax) {
+    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  }
 
   // Fungsi untuk mengganti status aktivitas secara otomatis
-  const toggleActivityServo = () => {
-    setTimeout(() => {
-      setIsActiveServo((prevIsActiveServo) => !prevIsActiveServo);
-      toggleActivityServo(); // Menerapkan rekursif untuk memicu perubahan setiap 4 detik
-    }, 200);
-  };
 
-  const toggleActivityLED = () => {
-    setTimeout(() => {
-      setIsActiveLED((prevIsActiveLED) => !prevIsActiveLED);
-      toggleActivityLED(); // Menerapkan rekursif untuk memicu perubahan setiap 4 detik
-    }, 200);
-  };
+  const onMessage = (message) =>{
+    if(message.topic == "hafidzganteng/irrigation"){
+      const intValue = parseInt(message.payloadString, 10);
+      setTargetHumidity(Math.round(map(intValue, 4096, 0, 0, 100)));
+    }
+    else if(message.topic == "hafidzganteng/servo"){
+      if(message.payloadString == "true"){
+        setIsActiveServo(true)
+      }
+      else{
+        setIsActiveServo(false)
+      }
+    }
+    else if(message.topic == "hafidzganteng/detectpest"){
+      if(message.payloadString == "y"){
+        setIsActiveLED(true)
+      }
+      else{
+        setIsActiveLED(false)
+      }
+    }
+    
+    
+  }
 
   // Panggil toggleActivity saat komponen pertama kali dimuat
   useEffect(() => {
-    toggleActivityServo();
-    toggleActivityLED();
+
+    //MQTT Over Websocket
+    const client = new Client("broker.hivemq.com", Number(8000), "ClientID");
+    // Check if the client is already connected
+    if (client.isConnected()) {
+      console.log("Already connected!");
+      return;
+    }
+    client.connect({
+      onSuccess: () => {
+        console.log("Connected!");
+        client.subscribe("hafidzganteng/servo");
+        client.subscribe("hafidzganteng/soilmoisture");
+        client.subscribe("hafidzganteng/irrigation");
+        client.subscribe("hafidzganteng/detectpest");
+        client.onMessageArrived = onMessage;
+      },
+      onFailure: () => {
+        console.log("Failed to connect!");
+      },
+    });
   }, []);
 
   return (
@@ -43,7 +80,7 @@ export default function ServoLEDMonitor() {
           Hardware Monitoring
         </div>
         <div className="grid grid-cols-2 space-x-2 md:space-x-8">
-          <CardComponents isActive={isActiveServo} type="servo" />
+          <CardComponents isActive={isActiveServo} type="servo" targetHumidity={targetHumidity} />
           <CardComponents isActive={isActiveLED} type="leduv" />
         </div>
       </div>
