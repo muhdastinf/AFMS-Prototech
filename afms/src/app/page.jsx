1a3ac6b177ec970f7e98699fc157a5d8f230f94d
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-03-04 20:21:27 +0700

    feat: add alert

--- afms/src/app/page.jsx
+++ afms/src/app/page.jsx
@@ -1,9 +1,7 @@
 "use client";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import { Sarabun } from "next/font/google";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
@@ -12,6 +10,8 @@ const sarabun = Sarabun({ weight: "800", subsets: ["latin"] });
 export default function Login() {
   const [MacAddress1, setMacAddress1] = useState();
   const [MacAddress2, setMacAddress2] = useState();
+  const [alert, setAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   const router = useRouter();
 
@@ -24,6 +24,8 @@ export default function Login() {
       Cookies.set("loggedmacaddress", "true");
       router.push("/home");
     } else {
+      setAlertMessage("401: Not Authenticated! Please Check Your Mac Address!");
+      setAlert(true);
     }
   };
   return (
@@ -79,6 +81,53 @@ export default function Login() {
           </div>
         </form>
       </div>
+      {alert ? (
+        <div
+          id="toast-danger"
+          class="fixed flex items-center w-full max-w-sm md:max-w-lg p-4 space-x-4 text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
+          role="alert"
+        >
+          <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+            </svg>
+            <span class="sr-only">Error icon</span>
+          </div>
+          <div class="ms-3 text-sm font-normal">{alertMessage}</div>
+          <button
+            type="button"
+            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-danger"
+            aria-label="Close"
+            onClick={() => setAlert(false)}
+          >
+            <span class="sr-only">Close</span>
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
