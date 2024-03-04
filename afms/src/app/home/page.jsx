21d8b2cb3c82109429a38f170d70ae2405cd4964
muhdastinfauzi <muhammaddastinfauzi@gmail.com>
2024-03-04 12:16:54 +0700

    feat: update

--- afms/src/app/home/page.jsx
+++ afms/src/app/home/page.jsx
@@ -3,7 +3,7 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import CardImage from "../components/CardImage";
-import style from "./page.module.css";
+import style from "../page.module.css";
 
 import { Yellowtail } from "next/font/google";
 
